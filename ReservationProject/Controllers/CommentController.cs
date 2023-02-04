d8e7d2b9e8fd8bfee28c3b052f89dda83b05ccdb
Burak337 <burak.alatas2011@gmail.com>
2023-02-04 14:07:12 +0300

    AspNet Core identity added to system and database.

---
+++ ReservationProject/Controllers/CommentController.cs
@@ -0,0 +1,27 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReservationProject.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+        
+        [HttpGet]
+        public PartialViewResult AddComment(int id)
+        {
+            ViewBag.id = id;
+            return PartialView();
+        }
+        [HttpPost]
+        public IActionResult AddComment(Comment comment)
+        {
+            comment.CommentDate = DateTime.Parse(DateTime.Now.ToShortDateString());
+            comment.CommentStatus = true;
+            commentManager.Add(comment);
+            return Redirect(Url.Action("DestinationDetails", "Destination", new { id = comment.DestinationID })+"#yorumlar");
+        }
+    }
+}
