f88a1a648f5d4a7b26157536d3f8e8a4a0e24a72
Burak337 <burak.alatas2011@gmail.com>
2023-02-28 12:27:08 +0300

    Sending mail system added to project (MailKit). Not active beacuse i didn't add mail address to system.

---
+++ ReservationProject/Areas/Admin/Controllers/MailController.cs
@@ -0,0 +1,51 @@
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using ReservationProject.Models;
+
+namespace ReservationProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MailController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(MailRequest mailRequest)
+        {
+            try
+            {
+                MimeMessage message = new MimeMessage();
+
+                MailboxAddress mailboxAddressFrom = new MailboxAddress("Admin", "Mail Yollayacağımız mail adresi");
+                message.From.Add(mailboxAddressFrom);
+
+                MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailRequest.ReceiverMail);
+                message.To.Add(mailboxAddressTo);
+
+                message.Subject = mailRequest.Subject;
+
+                var bodyBuilder = new BodyBuilder();
+                bodyBuilder.TextBody = mailRequest.Content;
+                message.Body = bodyBuilder.ToMessageBody();
+
+                SmtpClient client = new();
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate("Mail Yollayacağımız mail adresi", "gmail Tarafında Ayarları Yaptıktan Sonra Bize Verilece Kodu Buraya Girmeliyiz");
+                client.Send(message);
+                client.Disconnect(true);
+
+
+                return View();
+
+            }
+            catch (Exception)
+            {
+                return Content("Mail Gönderme Kısmı Henüz Aktif Edilmedi!");
+            }
+        }
+    }
+}
---
+++ ReservationProject/Models/MailRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace ReservationProject.Models
+{
+    public class MailRequest
+    {
+        public string Name { get; set; }
+        public string SenderMail { get; set; }
+        public string ReceiverMail { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+}
